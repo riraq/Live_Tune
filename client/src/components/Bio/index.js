dd1f038f5671224ef5334b9fe564574cb30bc121
riraq <riraq@mail.usf.edu>
2021-05-26 20:48:03 -0500

    added starter code to components

---
+++ client/src/components/Bio/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Bio(){
+  return(
+    <div>
+      
+    </div>
+  );
+}
+
+export default Bio
---
+++ client/src/components/CardWrapper/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function CardWrapper(){
+  return(
+    <div>
+      
+    </div>
+  );
+}
+
+export default CardWrapper
