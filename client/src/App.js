83d762d213aaa82a86673b1cae4b3cdd663d0e43
riraq <riraq@mail.usf.edu>
2021-06-01 16:39:13 -0500

    updated pages to display something; update packages/server

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Explore from "./pages/Explore";
+import Event from "./pages/Event";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/explore">
+            <Explore />
+          </Route>
+          <Route exact path="/event">
+            <Event />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+
+function Login() {
+
+  return (
+    <div>
+      This is the Login Page
+    </div>
+  )
+}
+
+export default Login;
