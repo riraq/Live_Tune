52691fd1c5bcc457f8e650f3f09d78287b9166c5
riraq <riraq@mail.usf.edu>
2021-06-01 15:12:00 -0500

    added dotenv and env file example for API calling

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const session = require('express-session');
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
