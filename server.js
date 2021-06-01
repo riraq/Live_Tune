600f4076d10a41a9b971dc5c25cedb90b07c1744
riraq <riraq@mail.usf.edu>
2021-06-01 18:57:43 -0500

    created model for users

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/liveTuneUsers",
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
