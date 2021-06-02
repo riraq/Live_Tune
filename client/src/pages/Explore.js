aeab48e83c1db5dc32f2940315c92c1a30440f25
riraq <riraq@mail.usf.edu>
2021-06-01 20:30:19 -0500

    ticketmaster API call now gets data to the frontend

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -2,10 +2,26 @@ import React, { useState, useEffect } from "react";
 import API from "../utils/API";
 
 function Explore() {
+  const [events, setEvents] = useState({})
+
+  useEffect(() => {
+  }, [])
+
+  function showState() {
+    init()
+    console.log("this is the events state: ", events)
+    }
+
+  function init() {
+    API.getEvents()
+    .then(res => setEvents(res.data))
+    .catch(err => console.log("Error: ", err))
+  }
 
   return (
     <div>
       This is the Explore Page!
+      <button onClick={showState}>console</button>
     </div>
   )
 }
