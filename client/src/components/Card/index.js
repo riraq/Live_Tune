69f4fe081aebb0a67d56f76864f7b16488b230cd
riraq <riraq@mail.usf.edu>
2021-06-02 14:38:28 -0500

    explore page shows events when button clicked

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,9 +1,23 @@
 import React from "react"
 
-function Card(){
-  return(
-    <div>
-      
+function Card(props) {
+  console.log("this is the card props", props)
+  const thumbnail = {
+    "width": "100px",
+    "height": "100px"
+  }
+  return (
+    <div className="row m-2">
+
+      <div className="col-4">
+        <img alt="concert poster" style={thumbnail} src={props.image} />
+      </div>
+      <div className="col-8">
+        <p>{props.name}</p>
+        <p>{props.date}</p>
+        <a href={props.url} target="blank"> Buy Tickets Here!</a>
+      </div>
+
     </div>
   );
 }
--- client/src/components/CardWrapper/index.js
+++ client/src/components/CardWrapper/index.js
@@ -1,9 +1,18 @@
 import React from "react"
+import Card from "../Card"
 
-function CardWrapper(){
-  return(
-    <div>
-      
+function CardWrapper(props) {
+  console.log("cardwrapper props", props)
+  return (
+    <div className="container-fluid">
+      <Card
+              key={props.id}
+              id={props.id}
+              name={props.name}
+              date={props.date}
+              image={props.image}
+              url={props.url}
+      />
     </div>
   );
 }
