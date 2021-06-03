8667431eaa0335c0ece883ec31ddeef9a8fc1eca
ManuelGF <manuelg139@gmail.com>
2021-06-03 14:11:39 -0500

    Created Header and Venue Card to be added into the event page and profile page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React from "react";
 
-function Header(){
-  return(
-    <div>
-      <p>user name to display</p>
-    </div>
-  );
+
+function VenueCard(props) {
+    return (
+        <div className="jumbotron text-center" style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", marginBottom:"2rem", backgroundImage: `url("")`, backgroundBlendMode:"hard-light"}}>
+            <div className="container text-dark">
+                <h1>{props.name}</h1>
+            </div>
+        </div>
+    );
 }
 
-export default Header
+export default VenueCard;
---
+++ client/src/components/VenueCard/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function VenueCard(props) {
+  const banner = {
+    "width": "80%",
+    "height": "300px"
+    `background`
+  }
+  return (
+    <div className="row m-2">
+      <div className="col-8" style={banner} >
+       <h3>{props.name}</h3>
+       <img alt="concert poster" ={props.image}src />
+      </div>
+     
+
+    </div>
+  );
+}
+
+export default VenueCard
