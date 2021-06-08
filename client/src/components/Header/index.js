b2f17e20fed28071dde28521c05aed0eb6991416
ManuelGF <manuelg139@gmail.com>
2021-06-07 21:54:46 -0500

    created a Spring module transaction

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header(props){
   return(
      <div className="jumbotron text-center" style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", marginBottom:"2rem", backgroundImage: `url("")`, backgroundBlendMode:"hard-light"}}>
             <div className="container text-dark">
-                <h1>{username}</h1>
+              
             </div>
      </div>
 
--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -1,18 +1,63 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API";
-import CardWrapper from "../components/CardWrapper"
+import CardWrapper from "../components/CardWrapper";
+import styles from '../styles.modules.css';
 
+import {
+  useTransition,
+  useSpring,
+  useChain,
+  config,
+  animated,
+  useSpringRef,
+} from '@react-spring/web';
 
 import Card from "../components/Card"
 import {Search, SearchBtn} from "../components/Search";
 
 
+
+
 function Explore() {
   const [events, setEvents] = useState({})
   const [searchForm, setsearchForm] = useState({})
+ 
+
+  //Spring Const 
+  const [open, set] = useState(false)
+  const springApi = useSpringRef()
+  const { size, ...rest } = useSpring({
+    ref: springApi,
+    config: config.stiff,
+    from: { size: '20%', background: 'hotpink' },
+    to: {
+      size: open ? '100%' : '20%',
+      background: open ? 'white' : 'hotpink',
+    },
+  })
+
+
+  const transApi = useSpringRef()
+  const transition = useTransition(open ? events : [], {
+    ref: transApi,
+    trail: 400 / events.length,
+    from: { opacity: 0, scale: 0 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0 },
+  })
+
+
 
   useEffect(() => {
   }, [])
+
+
+   // This will orchestrate the two animations above, comment the last arg and it creates a sequence
+   useChain(open ? [springApi, transApi] : [transApi, springApi], [
+    0,
+    open ? 0.1 : 0.6,
+  ])
+
 
   function showState() {
     searchEvents()
@@ -41,6 +86,8 @@ function Explore() {
 
   return (
     <div>
+
+      
       <div className="text-center">
         <h1>Explore</h1>
         <form>
@@ -57,8 +104,19 @@ function Explore() {
           </SearchBtn>
         </form>
       </div>
-      <div>
-        {events.length ?
+      <div className={styles.wrapper}>
+      <animated.div
+        style={{ ...rest, width: size, height: size }}
+        className={styles.container}
+        onClick={() => set(open => !open)}>
+        {transition((style, item) => (
+          <animated.div
+            className={styles.item}
+            style={{ ...style, background: item.css }}
+          />
+        ))}
+
+      {events.length ?
           (events.map(event => (
             (<CardWrapper
               key={event.id}
@@ -73,6 +131,10 @@ function Explore() {
             <div></div>
           )
         }
+
+      </animated.div>
+
+        
       </div>
     </div>
   )
