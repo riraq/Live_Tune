2afec9bd91ba1ee86d3ca15d253e1b1cd5b84baa
ManuelGF <manuelg139@gmail.com>
2021-06-12 10:51:46 -0500

    Log In Material UI Style

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,27 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignupForm"
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
 import "../index.css"
+
 
 
 function Login() {
 
   return (
-    <div className="container main-landing login" >
-        <LoginForm />
+
+    <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="sm" className=" main-landing login">
+
+   
+    <LoginForm />
         <SignUpForm />
-    </div>
+      </Container>
+    </React.Fragment>
   )
 }
 
