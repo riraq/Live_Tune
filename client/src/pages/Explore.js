f2952ef412bad7be709c7234c6e4474df59c4407
ManuelGF <manuelg139@gmail.com>
2021-06-12 13:17:08 -0500

    Explore Grid Structure Material

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -5,6 +5,10 @@ import Card from "../components/Card";
 import Nav from "../components/Nav";
 import { Search, SearchBtn } from "../components/Search";
 import { Link } from "react-router-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
 function Explore() {
   const [events, setEvents] = useState({})
@@ -29,13 +33,21 @@ function Explore() {
   };
 
   return (
-    <div>
-      <Nav />
-      <div className="text-center">
+    
+    <React.Fragment>
+    <CssBaseline />
+    <Container maxWidth="lg" className="main-wrapper ">
+
+
+
+    <Grid container spacing={3}>
+        <Grid item xs={12}>
+        <Nav />
+        </Grid>
+        <Grid item xs={12}>
         <h1>Explore</h1>
-        {/* <Link to={"/profile"}>
-          <button><strong>Profile Page</strong></button>
-        </Link> */}
+        </Grid>
+        <Grid item xs={12}>
         <form className="form-group">
           <Search
             onChange={handleInputChange}
@@ -49,10 +61,12 @@ function Explore() {
             Find Events
           </SearchBtn>
         </form>
-      </div>
+        </Grid>
+       
 
-
-      <div>
+    <Grid container spacing={3}>
+    <Grid item xs={12}>
+        <Grid item xs={3}>
         {events.length ? (
           (events.map(event => (
             <Card
@@ -67,9 +81,56 @@ function Explore() {
         ) : (
           <div></div>
         )}
-      </div>
-    </div>
+        </Grid> </Grid>
+        </Grid>
+        <Grid item xs={12}>
+        <h1>FOOTER</h1>
+        </Grid>
+      </Grid>
+   
+    </Container>
+    </React.Fragment>
   )
 }
 
 export default Explore;
+
+/* 
+<Nav />
+<div className="text-center">
+  <h1>Explore</h1>
+  { <Link to={"/profile"}>
+    <button><strong>Profile Page</strong></button>
+  </Link> }
+  <form className="form-group">
+    <Search
+      onChange={handleInputChange}
+      name="search"
+      placeholder="Search for an Event"
+    />
+    <SearchBtn
+      disabled={!(searchForm.search)}
+      onClick={handleFormSubmit}
+    >
+      Find Events
+    </SearchBtn>
+  </form>
+</div>
+
+
+<div>
+  {events.length ? (
+    (events.map(event => (
+      <Card
+        key={event.id}
+        id={event.id}
+        name={event.name}
+        date={event.dates.start.localDate}
+        image={event.images[4].url}
+        url={event.url}
+      >
+      </Card>)))
+  ) : (
+    <div></div>
+  )}
+</div> */
