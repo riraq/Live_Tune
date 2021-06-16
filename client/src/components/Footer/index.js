be424c588f72065572a220db29fe2477b3b6697b
ManuelGF <manuelg139@gmail.com>
2021-06-16 15:21:05 -0500

    Footer Info

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,11 +3,32 @@ import "./style.css"
 
 function Footer(){
   return (
+
+<Container maxWidth="lg" >
+<Grid item xs={12} >
+    <img className="user-image" src=".../assets/LIVE-TUNE-LOGO.png"/>
+    </Grid> 
+    <Grid item xs={12} >
+   <h3> Created by Rony Iraq, Manuel Frayre & Payton Whinnery</h3>
+      </Grid> 
+ </Container>
+
+
+      /* 
   <div>
+
+<Grid item xs={12} >
+    <img className="user-image" src=""/>
+    </Grid> 
+    <Grid item xs={12} >
+   <h3> Created by Rony Iraq, Manuel Frayre & Payton Whinnery</h3>
+      </Grid> 
       <div className="footer">
+
+        
       Created by Rony Iraq, Manuel Frayre & Payton Whinnery
       </div>
-  </div>
+  </div> */
   )
 }
 
